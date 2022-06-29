087b829d9a1705a9751284c9e6a8f6b674c2a906
Iryna Zubrytska <zubrytska.ira@gmail.com>
2022-06-29 09:50:21 +0200

    added welcome screen

---
+++ ML_AR_FlowerRecognizer/OrientationExtension.swift
@@ -0,0 +1,30 @@
+//
+//  OrientationExtension.swift
+//  ML_AR_FlowerRecognizer
+//
+//  Created by Iryna Zubrytska on 29.06.2022.
+//
+
+import Foundation
+import CoreML
+import UIKit
+
+extension CGImagePropertyOrientation {
+    /// Converts an image orientation to a Core Graphics image property orientation.
+    /// - Parameter orientation: A `UIImage.Orientation` instance.
+    ///
+    /// The two orientation types use different raw values.
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+            case .up: self = .up
+            case .down: self = .down
+            case .left: self = .left
+            case .right: self = .right
+            case .upMirrored: self = .upMirrored
+            case .downMirrored: self = .downMirrored
+            case .leftMirrored: self = .leftMirrored
+            case .rightMirrored: self = .rightMirrored
+            @unknown default: self = .up
+        }
+    }
+}
--- ML_AR_FlowerRecognizer/WelcomeViewController.swift
+++ ML_AR_FlowerRecognizer/WelcomeViewController.swift
@@ -1,17 +1,17 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  ML_AR_FlowerRecognizer
 //
 //  Created by Iryna Zubrytska on 29.06.2022.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 }
-
